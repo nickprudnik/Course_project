566ea27de9c78c5c5430b3d2bb47e70ad401a82b
temaand1 <45510216+temaand1@users.noreply.github.com>
2019-01-26 18:26:41 +0300

    NEWEST 2

--- script.js
+++ script.js
@@ -9,23 +9,53 @@ x.className += " responsive";
 x.className = "menu-main"; 
 } 
 };
-function store(){
+document.getElementById("submit").onclick = function (){
+    let error = false;
+    document.getElementById("Name").classList.remove("error");
+    document.getElementById("errorName").style.display = "none";
+    document.getElementById("Surname").classList.remove("error");
+    document.getElementById("errorSurname").style.display = "none";
+    document.getElementById("Email").classList.remove("error");
+    document.getElementById("errorEmail").style.display = "none";
+    document.getElementById("Number").classList.remove("error");
+    document.getElementById("errorNumber").style.display = "none";
     let inputName= document.getElementById("Name");
-    localStorage.setItem("Name", inputName.value);
-    let inputSurname= document.getElementById("Surname");
-    localStorage.setItem("Surname", inputSurname.value);
-    let inputEmail= document.getElementById("Email");
-    localStorage.setItem("Email", inputEmail.value);
-    let inputNumber= document.getElementById("Number");
-    localStorage.setItem("Number", inputNumber.value);
-    let inputComments= document.getElementById("Comments");
-    localStorage.setItem("Comments", inputComments.value);
-    let input = document.querySelectorAll("#Name, #Surname, #Email, #Number, #Comments");
-    if (input.value !== '') {
+    if (inputName.value.length < 2){
+        error = true;
+        document.getElementById("Name").classList.add("error");
+        document.getElementById("errorName").style.display = "block"
+    }
+    let inputSurname = document.getElementById("Surname");
+    if (inputSurname.value.length < 3){
+        error = true;
+        document.getElementById("Surname").classList.add("error");
+        document.getElementById("errorSurname").style.display = "block"
+    }
+    let inputEmail = document.getElementById("Email");
+    if (inputEmail.value.length < 3){
+        error = true;
+        document.getElementById("Email").classList.add("error");
+        document.getElementById("errorEmail").style.display = "block"
+    }
+    let inputNumber = document.getElementById("Number");
+    if (inputNumber.value.length < 3){
+        error = true;
+        document.getElementById("Number").classList.add("error");
+        document.getElementById("errorNumber").style.display = "block"
+    }
+    if (!error){
+        localStorage.setItem("Name", inputName.value);
+        localStorage.setItem("Surname", inputSurname.value);
+        localStorage.setItem("Email", inputEmail.value);
+        localStorage.setItem("Number", inputNumber.value);
         let storedValue = `${localStorage.getItem("Name")} ваша заявка принята`;
         document.getElementById("request_name").innerHTML = storedValue;
-    } else {
-        let not_storedval = `Заполните пожалуйста форму`;
-        document.getElementById("not_request").innerHTML = not_storedval;
+        document.getElementById("openModal").style.display = "block"
     }
-}
+};
+document.getElementById("close").onclick = function (){
+    document.getElementById("openModal").style.display = "none"
+};
+document.getElementById("too_close").onclick = function (){
+    document.getElementById("openModal").style.display = "none"
+};
